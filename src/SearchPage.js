a2b8cc252a53307fb50c77e75ad8c57292ad218a
Mike Tikh <mike.tikh@gmail.com>
2018-01-25 14:22:52 -0600

    Add link to SearchPage
    Routes "close search" to bring you back to homepage

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import * as BooksAPI from './BooksAPI'
 import SearchResults from './SearchResults'
+import { Link } from 'react-router-dom'
 
 class SearchPage extends Component {
     state = {
@@ -20,7 +21,7 @@ class SearchPage extends Component {
         return (
         <div className="search-books">
           <div className="search-books-bar">
-            <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+            <Link to="/" className="close-search">Close</Link>
             <div className="search-books-input-wrapper">
               {/*
                 NOTES: The search from BooksAPI is limited to a particular set of search terms.
