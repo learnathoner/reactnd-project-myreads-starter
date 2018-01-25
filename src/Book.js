f05c7c5eb803e00543508d2f9cb46e0633ca88b0
Mike Tikh <mike.tikh@gmail.com>
2018-01-24 19:28:09 -0600

    Update Book Component
    Tested Book component, able to load in book information from props

--- src/Book.js
+++ src/Book.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 
 class Book extends Component {
-    render({ book }) {
+    render() {
+        const book = this.props.book
+
         return (
             <div className="book">
                 <div className="book-top">
                     <div className="book-cover"
                         style={{ width: 128,
                                 height: 193,
-                                backgroundImage: { book.imageLinks.smallThumbnail }}
-                            }
+                                backgroundImage: `url("${book.imageLinks.smallThumbnail}")` }}
                     ></div>
                     <div className="book-shelf-changer">
                         <select>
@@ -24,7 +25,6 @@ class Book extends Component {
                 <div className="book-title">{ book.title }</div>
                 <div className="book-authors">{ book.authors.join(', ')}</div>
             </div>
-
         )
     }
 }
